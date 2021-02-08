8cea59f2541fbb8598a409437ec5885504036710
Nazarich <nazariykoshytskiy@gmail.com>
2021-02-08 09:15:15 +0200

    initial commit

---
+++ code/index.php
@@ -0,0 +1,6 @@
+<?
+use clases\Clients;
+require_once __DIR__.'/vendor/autoload.php';
+$re= new Clients("https://newaccount1612559556448.freshdesk.com","QsoKVke1gL5e05IR9Dr");
+$re->createCsv();
+//var_dump($re);
